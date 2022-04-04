f5d3ca7f97399c099211c8d3be0f48c7ace6e149
mengiefen <mengiefen@gmail.com>
2022-04-05 00:30:41 +0300

    add header component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Rockets from './components/Rockets/Rockets';
+import Missions from './components/Missions/Missions';
+import MyProfile from './components/MyProfile/MyProfile';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route to="/" element={<Rockets />} />
+        <Route to="missions" element={<Missions />} />
+        <Route to="my-profile" element={<MyProfile />} />
+      </Routes>
     </div>
   );
 }
