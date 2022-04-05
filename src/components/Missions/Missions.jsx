7f7cd0d2c7fb5750da56c131bbec282a43bba72a
Francisco Ponce <francisco.ponce.dev@gmail.com>
2022-04-05 12:27:32 -0300

    create mission and mission item

--- src/components/Missions/Missions.jsx
+++ src/components/Missions/Missions.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import MissionItem from './MissionItem';
 
-function Missions() {
-  return <div>Missions</div>;
-}
+const Missions = () => {
+  const missionList = useSelector((state) => state.missionsReducer);
+
+  return (
+    <section className="missions-container">
+      <table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Mission</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th> </th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            missionList.map((mission) => (
+              <MissionItem
+                key={mission.id}
+                id={mission.id}
+                name={mission.name}
+                description={mission.description}
+              />
+            ))
+          }
+        </tbody>
+      </table>
+    </section>
+  );
+};
 
 export default Missions;
