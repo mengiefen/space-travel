9b69a92dfc16935a72ed51e06c2011045ec41dbc
mengiefen <mengiefen@gmail.com>
2022-04-05 16:43:55 +0300

    update index.js file with store provider and React 18  render methods

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/rockets/rocketsSlice';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>,
-  document.getElementById('root'),
 );
