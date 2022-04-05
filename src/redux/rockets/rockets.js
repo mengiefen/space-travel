6a5313b2b3d72eed0827ae7a5bb1d21df53d7843
mengiefen <mengiefen@gmail.com>
2022-04-05 16:41:47 +0300

    add rocketsReducer and the corresponding actions

---
+++ src/redux/rockets/rockets.js
@@ -0,0 +1,72 @@
+import { getAllRocketsFromAPI } from '../apiCalls/apiCalls';
+
+const GET_ROCKETS = 'spaceTravel/Rockets/GET_ROCKETS';
+const RESERVE_ROCKETS = 'spaceTravel/Rockets/RESERVE_ROCKETS';
+const CANCEL_RESERVED_ROCKET = 'spaceTravel/Rockets/CANCEL_RESERVED_ROCKET';
+
+const initialState = [];
+
+const rocketsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_ROCKETS:
+      return action.rockets;
+    case RESERVE_ROCKETS:
+      return state.map((rocket, index) => {
+        if (index !== action.id) {
+          return rocket;
+        }
+        return {
+          ...rocket,
+          reserved: true,
+        };
+      });
+
+    case CANCEL_RESERVED_ROCKET:
+      return state.map((rocket, index) => {
+        if (index !== action.id) {
+          return rocket;
+        }
+        return {
+          ...rocket,
+          reserved: false,
+        };
+      });
+
+    default:
+      return state;
+  }
+};
+
+export const getRocketsList = (rockets) => ({
+  type: GET_ROCKETS,
+  rockets,
+});
+
+export const reserveRocket = (id) => ({
+  type: RESERVE_ROCKETS,
+  id,
+});
+
+export const cancelReservedRocket = (id) => ({
+  type: CANCEL_RESERVED_ROCKET,
+  id,
+});
+
+export const getAllRockets = () => async (dispatch) => {
+  const rocketsFromAPI = await getAllRocketsFromAPI();
+  const rockets = [];
+  rocketsFromAPI.forEach((rocket) => {
+    rockets.push({
+      id: rocket.id - 1,
+      rocket_id: rocket.rocket_id,
+      description: rocket.description,
+      name: rocket.rocket_name,
+      rocket_img: rocket.flickr_images,
+      reserved: false,
+    });
+  });
+
+  dispatch(getRocketsList(rockets));
+};
+
+export default rocketsReducer;
