6d2934721055b89d6998992b972ead1c316f6209
Francisco Ponce <francisco.ponce.dev@gmail.com>
2022-04-05 21:42:39 -0300

    missions join/leave in profile

---
+++ src/components/MissionItem/MissionItem.jsx
@@ -0,0 +1,62 @@
+import { PropTypes } from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { joinMission, leaveMission } from '../../redux/missions/missions';
+import Button from 'react-bootstrap/Button';
+import './MissionItem.scss'
+
+const MissionItem = (props) => {
+  const dispatch = useDispatch();
+  const {
+    id, name, description, reserved,
+  } = props;
+
+  const handleJoinMission = () => {
+    dispatch(joinMission(id));
+  };
+
+  const handleLeaveMission = () => {
+    dispatch(leaveMission(id));
+  };
+
+  return (
+    <tr id={id}>
+      <td className="td-name">{name}</td>
+      <td className="td-description">{description}</td>
+      <td className="td-badge">
+        {reserved ? (
+          <Button variant="primary" className="btn-reserved" disabled>ACTIVE MEMBER</Button>
+        ) : (
+          <Button variant="secondary" className="btn-reserved">NOT A MEMBER</Button>
+        )}
+      </td>
+      <td className="td-button">
+        {reserved ? (
+          <Button
+            type="button"
+            variant="secondary"
+            onClick={handleLeaveMission}
+          >
+            Leave Mission
+          </Button>
+        ) : (
+          <Button
+            type="button"
+            variant="outline-secondary"
+            onClick={handleJoinMission}
+          >
+            Join Mission
+          </Button>
+        )}
+      </td>
+    </tr>
+  );
+};
+
+MissionItem.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  reserved: PropTypes.bool.isRequired,
+};
+
+export default MissionItem;
--- src/redux/rockets/rocketsSlice.js
+++ src/redux/rockets/rocketsSlice.js
@@ -1,12 +1,15 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+import missionsReducer, { fetchMissionsFromAPI } from '../missions/missions';
 import rocketsReducer from './rockets';
 
 const rootReducer = combineReducers({
   rockets: rocketsReducer,
+  missionsReducer,
 });
 
 const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+store.dispatch(fetchMissionsFromAPI());
 
 export default store;
